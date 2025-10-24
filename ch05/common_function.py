5041e8a019c41603d5dec46a2c9a48fddd77159a
214 <wahfkhaweiofhioehfwiohfao@dfjiaojfoia>
2025-10-24 14:14:00 +0900

    added ch08 (sample 01,02,03)

--- ch05/common_function.py
+++ ch05/common_function.py
@@ -1,5 +1,8 @@
 import platform
 
+from matplotlib import pyplot as plt
+from matplotlib import rc
+
 #현재 파이썬이 실행되는 플랫폼 확인
 
 def is_windows_platform():
@@ -19,4 +22,7 @@ def get_font_name():
     else:
         return 'Malgun Gothic'
 
-
+def init_matplotlib():
+    # 한글폰트 처리(깨짐처리)
+    rc('font', family=get_font_name())
+    plt.rcParams['axes.unicode_minus'] = False
